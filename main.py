4cacfe7e4e317804bff475878c529e9a8d30f9b3
Francesco Silvani <83598456+Totallynotnasaboi@users.noreply.github.com>
2021-09-01 10:32:38 +0000

    Drawn image to the screen

--- main.py
+++ main.py
@@ -23,9 +23,13 @@ def player():
 # Game Loop
 running = True
 while running:
+  
+  # RGB = Red, Green, Blue
+  screen.fill((0, 0, 0))
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
   
-  screen.fill((0, 0, 0))
+  player()
   pygame.display.update()
